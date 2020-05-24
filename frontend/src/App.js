8d2afd399ef5dd88b1ef0a1f30239ac752c70397
panda <kongfupanda2050@gmail.com>
2020-05-24 17:41:10 +0200

    redux setup for authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './hocs/Layout'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import About from './containers/About';
+import Listing from './containers/Listing';
+import Signup from './containers/Signup';
+import Login from './containers/Lognin';
+import ListingDetail from './containers/ListingDetail';
+import Contact from './containers/Contact';
+import NotFound from './components/NotFound'
+import './saas/main.scss'
+import { Provider } from 'react-redux';
+import store from './store';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/listing' component={Listing} />
+            <Route exact path='/listing/:id/' component={ListingDetail} />
+            <Route exact path='/login/' component={Login} />
+            <Route exact path='/signup/' component={Signup} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ frontend/src/containers/ListingDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ListingDetail = () => {
+    return(
+        <div>
+            ListingDetail
+        </div>
+    )
+}
+export default ListingDetail;
